7eb71ed0d99fa4a0757eeac4fde2005868df29bf
Songlcy <songxlcy@gmail.com>
2020-02-28 14:24:11 +0800

    test2

--- lib/model/WeatherModel.dart
+++ lib/model/WeatherModel.dart
@@ -2,9 +2,9 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class WeatherModel with ChangeNotifier {
+
   String _count = "明日天气";
   String get count => _count;
-
   void requestWeatherInfo() {
     notifyListeners();
   }
